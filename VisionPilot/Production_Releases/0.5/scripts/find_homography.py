9b1fd7dec5c71f44d53d3c7a82a433b1eaa963bf
TranHuuNhatHuy <huy9515@gmail.com>
2025-11-23 11:12:32 +0900

    found 4 raw coords of source points

--- VisionPilot/Production_Releases/0.5/scripts/find_homography.py
+++ VisionPilot/Production_Releases/0.5/scripts/find_homography.py
@@ -1,5 +1,6 @@
 import cv2
 
+
 """
 
 This script finds the homography matrix to transform a cropped image of a road scene
@@ -14,21 +15,29 @@ Standard frame:
 
 """
 
-# INPUT : cropped-only image (2880 x 1440)
 
-# 1. Read raw image (2880 x 1860)
+class BEVHomography:
+    def __init__(self):
 
-# 2. Crop raw image (2880 x 1440)
+        # Load standard frame image
+        self.standard_frame = cv2.imread(
+            "./assets/standard_frame.jpg"
+        )
 
-# 3. Find source points and destination points. Basically I can put a single perfect frame here and manually select the points.
+        # MANUALLY DEFINED SOURCE POINTS
+        # Define manually 4 source points by picking the best frame where car 
+        # is perfectly straight (so egoleft/right make perfect trapezoid)
+        # Four source points will be in the order: 
+        # - Left bottom
+        # - Right bottom
+        # - Left top
+        # - Right top
+        # Supported by GIMP to get pixel coordinates
+        RAW_SRC_POINTS = [
+            (448  , 1859),  # Left bottom
+            (2108 , 1859),  # Right bottom
+            (1380 , 1049),  # Left top
+            (1488 , 1049)   # Right top
+        ]
 
-# 4. Define manually 4 source points by picking the best frame where car is perfectly straight (so egoleft/right make perfect trapezoid)
-# Four source points will be in the order: left bottom, right bottom, left top, right top
-# Must be normalized coords (0, 1)
-
-# 5. Set destination points
-# From a BEV grid of 640 x 640
-# Four destination points will be in the order: left bottom (159, 639), right bottom (479, 639), left top (159, 0), right top (479, 0)
-# They are NOT normalized
-
-# OUTPUT : BEV grid with straight lane markings (not picture, just the mask itself)
+        
