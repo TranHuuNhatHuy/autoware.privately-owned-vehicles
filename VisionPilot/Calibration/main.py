be5a081205cf1c5673c49db2e05a4cc3689531fd
TranHuuNhatHuy <huy9515@gmail.com>
2026-03-19 10:16:42 +0700

    add image undistortion func to main

--- VisionPilot/Calibration/main.py
+++ VisionPilot/Calibration/main.py
@@ -20,6 +20,16 @@ def get_rotation_matrix(
 ):
     """
     Convert Euler angles (in degrees) to a rotation matrix.
+
+    Parameters:
+        - pitch_deg: pitch (x-angle rotation), in degrees
+        - yaw_deg: yaw (y-angle rotation), in degrees
+        - roll_deg: roll (z-angle rotation), in degrees
+
+    Returns:
+        - R: a 3x3 rotation matrix that later can be used to
+             warp the original frame to match standard pose
+             perspective.
     """
 
     pitch, yaw, roll = np.radians([
@@ -36,3 +46,34 @@ def get_rotation_matrix(
     R, _ = cv2.Rodrigues(rot_vector)
 
     return R
+
+
+def undistort_image(
+        image: np.ndarray,
+        intrinsic_matrix: np.ndarray,
+        distortion_coeffs: np.ndarray
+):
+    """
+    Undistort a front camera image using its specific
+    intrinsics matrix and distortion coefficients.
+
+    Parameters:
+        - image: original image, fetched via OpenCV library.
+        - intrinsic_matrix: a 3x3 matrix containing front camera's
+                            focal lengths and principal point (should
+                            be clearly defined in the camera config).
+        - distortion_coeffs: a vector containing front camera distortion
+                             coefficients (should also be defined in
+                             the camera config too).
+
+    Returns:
+        - undistorted_image: yeah as the name suggests lol.
+    """
+
+    undistorted_image = cv2.undistort(
+        image,
+        intrinsic_matrix,
+        distortion_coeffs
+    )
+
+    return undistorted_image
